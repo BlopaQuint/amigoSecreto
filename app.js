26613c0421c810ec120fbb6356051dce8faa7a9e
Pablo Quintiliano <pabloquintiliano@live.com>
2025-01-28 22:31:36 -0300

    Implementar código para adicionar nomes à lista.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+
+function adicionarAmigo() {
+    let amigo = document.querySelector("input").value;
+    if (amigo == "") {
+        alert(`Por favor, insira um nome no campo "Digite um nome" abaixo.`);
+    } else {
+        listaDeAmigos.push(amigo);
+    }
+    limparCampo();
+    console.log(listaDeAmigos);
+}
+
+
+function limparCampo() {
+    amigo = document.querySelector("input");
+    amigo.value = "";
+}
